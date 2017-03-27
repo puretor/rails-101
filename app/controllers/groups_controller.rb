2fe6d6364b9f0cf998fcc54fa893990b8c82feaa
puretor <mickie0306@gmail.com>
2017-03-28 02:26:47 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -44,27 +44,27 @@ class GroupsController < ApplicationController
   end
 
   def join
-     @group = Group.find(params[:id])
+   @group = Group.find(params[:id])
 
-      if !current_user.is_member_of?(@group)
-        current_user.join!(@group)
-        flash[:notice] = "加入本讨论版成功！"
-      else
-        flash[:warning] = "你已经是本讨论版成员了！"
-      end
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版成员了！"
+    end
 
-      redirect_to group_path(@group)
+    redirect_to group_path(@group)
   end
 
   def quit
-      @group = Group.find(params[:id])
+    @group = Group.find(params[:id])
 
-      if current_user.is_member_of?(@group)
-        current_user.quit!(@group)
-        flash[:alert] = "已退出本讨论版！"
-      else
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
         flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
-      end
+    end
 
       redirect_to group_path(@group)
   end
